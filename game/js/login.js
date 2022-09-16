e3f31a463c5a49149e92679cdae6e6eef9580797
Jerffeson Fialho <jerffesonfialho@gmail.com>
2022-09-16 15:08:49 -0300

    Primeira página
    Criação da página de login, com reset do CSS e estilização da página. JS para a interação com p button e input

---
+++ game/js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+
+const form = document.querySelector('.login-form')
+
+
+const validateInput = ({target}) => {
+    if (target.value.length > 2) {
+        button.removeAttribute ('disabled')
+    } else {
+        button.setAttribute ('disabled', '')
+    };
+}
+
+const handleSubmit = (event) => {
+     event.preventDefault();
+     
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
