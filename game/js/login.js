82c639598659473061f6c1ec724a84a297875f5f
Jerffeson Fialho <jerffesonfialho@gmail.com>
2022-09-19 17:37:29 -0300

    Finalizado a parte de CSS
    Finalizado a parte de CSS da página do jogo, a partir daqui vamos começar a fazer a parte js da página 2 (página com o jogo)

--- game/js/login.js
+++ game/js/login.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('.login_input');
 const button = document.querySelector('.login_button');
-const form = document.querySelector('.login-form')
+const form = document.querySelector('.login_form');
 
 
 const validateInput = ({target}) => {
@@ -12,8 +12,9 @@ const validateInput = ({target}) => {
 }
 
 const handleSubmit = (event) => {
-     event.preventDefault();
-     
+    event.preventDefault();
+    localStorage.setItem('player', input.value);
+    window.location = "./game.html";
 }
 
 input.addEventListener('input', validateInput);
